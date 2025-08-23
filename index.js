588c89751c1848798738836cd27b7d5adf4e41ea
さんたま <115996781+saintmaria000@users.noreply.github.com>
2025-08-24 00:26:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -24,19 +24,20 @@ app.post('/send', async (req, res) => {
     await transporter.sendMail({
         from: process.env.SMTP_USER, // ← 送信者（あなた）
         to: email, // ← フォーム入力者宛に送る
-        subject: '8/３０.Invite Ticket',
+        subject: `8/３０.Invite Ticket`,
         html: `
         <h1>こんにちは${name} さん\n\n
         あなたを Event にご招待します。</h1>
         <img src="cid:invite@aff" />
         `,
-        text: '8/30.AFF\n\n',
+        text: `8/30.AFF\n\n`,
         attachments: [
           {
             filename: 'flyer.jpg',
             path: path.join(__dirname, 'images', 'flyer.jpg'),
             cid: 'invite@aff', // 本文内のsrcと一致させる
           },
+        ]
     }); 
 
     res.status(200).send({ success: true });
