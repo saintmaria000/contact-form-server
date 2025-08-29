fa034f8befb50d5a9442fcfc1501eaa7de5d2d94
さんたま <115996781+saintmaria000@users.noreply.github.com>
2025-08-29 23:06:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.post('/send', async (req, res) => {
   const { name, email } = req.body;
 
   try {
-    const response = await fetch("https://api.maileroo.com/api/v2/emails", {
+    const response = await fetch("https://api.maileroo.com/send", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
